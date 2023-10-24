b30e7a2f2d7c58587220039cca8edc82841922e8
Toàn <142808050+ToanPham2000@users.noreply.github.com>
2023-10-24 11:49:37 +0700

    Update relay_ky_thuat_so.ino

--- relay_ky_thuat_so.ino
+++ relay_ky_thuat_so.ino
@@ -1,86 +1,117 @@
-#include <LiquidCrystal_I2C.h>//thư viện LCD
-LiquidCrystal_I2C lcd(0x27, 20, 4);//khởi tạo thư viện với số lượng chân giao diện
 
-#define Current_sensor A0
-#define PI 3.1416  
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,20,4); 
 
-float i,irms,Vadc,Vthat,t,t1,c;
-float sensor = 0.1;//Độ nhạy ACS712 20A = 0.1 V/A
-float offset = 512;//Giá trị bit điện áp đầu ra của ACS khi dòng điện bằng 0
-float m = 0, sum = 0;
-double deltaT = 0.02;
-double Ip = 0.4;//Lấy giá trị I pick up bằng 0.4 A
+const int acs712 = 0;
+const int Relay = 12;
 
-void setup() {
-Serial.begin(9600);
-pinMode(Current_sensor, INPUT);
-lcd.begin(20, 4); 
-pinMode(13, OUTPUT);
+int sampleCount = 0;
+int number_of_sample =  1000;     //số mẫu cần lấy 
+float k = 0.0048828125; //5:1024
+float counter = 0;
+float voltage = 0;
+float sum = 0;
+float sensitivity = 0.066;    // độ nhạy
+float Ip  = 1;          //dòng tác động quá tải
+float Inm = 10;      //dòng tác động ngắn mạch
+float Irms = 0;
+float check = 0;
+float t_trip  = 0;
+float t_res = 0;
+
+static bool error  =   false;
+float A = 0.052;
+float B = 0.114;
+float p = 0.02;
+float Tr  = 4.85;
+float m1=0;
+float delta=328;     //Chu kỳ tính (ms)
+ 
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(acs712, INPUT);
+  pinMode(Relay, OUTPUT);
+  digitalWrite(Relay, LOW);
+
+  lcd.init();                    
+  lcd.backlight();
+  m1=millis();
+
+}
+ 
+void loop(){
+  // Tính dòng hiệu dụng
+  voltage = analogRead(acs712)*k-2.5;
+  if  (voltage > 0){
+    sum +=  voltage; 
+  }
+  else{
+    sum -=  voltage;
+  }
+
+  sampleCount++;
+  if  (sampleCount  ==  number_of_sample ){
+    m1=millis();
+    Irms = 3.1416*sum/(2*sqrt(2)*sampleCount*sensitivity);
+    if(!error){
+      lcd.setCursor(0,0);
+      lcd.print("Irms = ");
+      lcd.setCursor(7,0);
+      lcd.print(Irms,3);
+      lcd.setCursor(12,0);
+      lcd.print("A");
+      lcd.setCursor(1,1);
+      lcd.print("C = ");
+      lcd.print(counter,3);
+    }
+  }
+  // Tính thời gian tác động
+  if(!error){
+    if  (sampleCount  ==  number_of_sample ){
+    if((Irms  > Ip) && (Irms<Inm)){
+      t_trip = A/(pow(Irms/Ip,p)-1)+B;
+      counter += delta/(t_trip*1000);
+    }
+    if(Irms  <= Ip){
+      if(counter!=0){
+          t_res = Tr/(1-Irms*Irms/Ip);
+          counter -=  delta/(t_res*1000);
+      }  
+    }
+    sum  = 0;
+    sampleCount = 0;
+    }
+     if(Irms >= Inm){
+      digitalWrite(Relay,HIGH);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("NGAN MACH ");
+      lcd.setCursor(0,1);
+      lcd.print("CAT NGAY ");
+      error=true;
+    }
+    if(counter  < 0){
+      counter = 0;
+    }
+    if(counter>=1){
+      digitalWrite(Relay,HIGH);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("QUA TAI ");
+      lcd.print(Irms,2);
+      lcd.print("A");
+      lcd.setCursor(0,1);
+      lcd.print("cat sau ");
+      lcd.print(t_trip,3);
+      lcd.print("s");
+      lcd.setCursor(0,2);
+      lcd.print("C = ");
+      lcd.print(counter,3);
+      error = true;
+    }
+  }
 }
 
-void loop() {
-  int bt = digitalRead(6);
-  Vadc = analogRead(Current_sensor);//giá trị analog
-  Vthat = (Vadc - offset);//trừ giá trị điện áp nguồn để ra giá trị điện áp thực
-  i = (Vthat*5.0/1024)/sensor ;//tính giá trị dòng điện thực
-  sum += abs(i);//tính tổng dòng điện
-  ++m;
-  if(m == 30){
-    irms = PI/(2*m*sqrt(2))*sum; //tính dòng điện hiệu dụng i với số lần lấy mẫu: 2m = 60 lần
-    m = 0;
-    sum = 0;
-    }   
-    lcd.backlight() ; 
-  lcd.display();
-  lcd.setCursor(0,0);
-  lcd.print("I = ");
-  lcd.setCursor(4,0);
-  lcd.print(irms,3);//Hiển thị 3 chữ số thập phân sau dấu phẩy
-  lcd.setCursor(10,0);
-  lcd.print("A");
-
- //Đặc tính cắt
-  double k = irms/Ip;  
-  if (k >= 2){
-    digitalWrite(13,HIGH);
-        lcd.clear();
-        lcd.display();
-        lcd.setCursor(5,1);
-        lcd.print("NGAN MACH");
-        }
-  if (1 < k && k < 2){
-  t = (0.052/(pow(k,0.02) - 1) + 0.114);//chọn đặc tính tác động chậm
-  }
-  if (1 > k && k >= 0 && t != 0){
-  t = 4.85/(pow(k,2) - 1);//thời gian nghỉ
-  }
-   if(t != 0){ 
-     c += deltaT/t;//hệ số tích lũy c
-     }
-   if(c < 0) c = 0;
-   if(c > 1){
-        t1 = t;
-        digitalWrite(13,HIGH);
-        lcd.display();
-        lcd.setCursor(5,1);
-        lcd.print("SOS: QUA TAI");     
-        lcd.display();
-        lcd.setCursor(1,3);
-        lcd.println("TIME");
-        lcd.setCursor(10,3);
-        lcd.print(t1,4);
-        lcd.setCursor(17,3);
-        lcd.print("s"); 
-        c = 0;
-      }
-lcd.setCursor(0,2);
-lcd.println("c =");
-lcd.setCursor(4,2);
-lcd.print(c,3);
-if(bt == HIGH){
-     lcd.clear();
-     digitalWrite(13,LOW);
-     c = 0;
-     t = 0;
-  }
-}
